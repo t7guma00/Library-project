5db3405b24861e6c35b1b49150695853a400b946
Unknown <kresteva.astana@gmail.com>
2018-11-15 00:56:10 +0200

    NavBar with Reactstrap
    Navbar with react strap in Header.js + Bootsrap, Query, and reacstrap packages

--- library-project/src/Views/Home.js
+++ library-project/src/Views/Home.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
-//import Header from '../Components/Header';
+import Header from '../Components/Header';
 //import Footer from '../Components/Footer';
 export default class Home extends Component {
-    state = { 
+    state = {
     }
-    render () {                                   
+    render () {
        return (
          <div id='container'>
+         <Header/>
          <div id='body'>
          <h3>Title
                     </h3>
-                    <p>Here is a description of how 
+                    <p>Here is a description of how
                      the app works </p>
           </div>
          </div>
